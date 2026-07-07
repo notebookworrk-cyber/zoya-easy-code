42e6e8608979d107a15b293a325ed86edeba1dc6
Lucky <notebookworrk@gmail.com>
2026-07-07 13:37:01 +0530

    docs: add module docstring to stdlib/json.py

--- zoya/stdlib/json.py
+++ zoya/stdlib/json.py
@@ -1,3 +1,5 @@
+"""Zoya stdlib JSON module."""
+
 from __future__ import annotations
 
 import json as _json
@@ -9,9 +11,7 @@ def load_module(interpreter: Any) -> Any:
 
     funcs = {
         "load": lambda path: _json.load(open(path, encoding="utf-8")),
-        "save": lambda data, path: _json.dump(
-            data, open(path, "w", encoding="utf-8"), indent=2
-        ),
+        "save": lambda data, path: _json.dump(data, open(path, "w", encoding="utf-8"), indent=2),
         "dumps": lambda data: _json.dumps(data, indent=2),
         "loads": lambda s: _json.loads(s),
     }
