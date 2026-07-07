13bd727fff277ca08d8faaeadc2d96a4e960e12d
Lucky <notebookworrk@gmail.com>
2026-07-07 13:42:32 +0530

    docs: add module docstring to zoya/security/__init__.py

--- zoya/security/__init__.py
+++ zoya/security/__init__.py
@@ -1,13 +1,8 @@
+"""Security package providing encryption, hashing, and input validation."""
+
 from .encryption import AESCipher, Hasher, KeyGenerator
 from .validation import Sanitizer, Validator
 
 __version__ = "0.1.0"
 
-__all__ = [
-    "AESCipher",
-    "Hasher",
-    "KeyGenerator",
-    "Validator",
-    "Sanitizer",
-    "__version__",
-]
+__all__ = ["AESCipher", "Hasher", "KeyGenerator", "Validator", "Sanitizer", "__version__"]
